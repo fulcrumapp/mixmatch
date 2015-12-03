918587ef5b389c527c1e2e8924d2c6b2d8ab7d2f
Zac McCormick <zac.mccormick@gmail.com>
2015-12-03 11:05:22 -0500

    Build

--- dist/index.js
+++ dist/index.js
@@ -9,79 +9,62 @@ Object.defineProperty(exports, "__esModule", {
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /* eslint no-invalid-this: 0*/
-/* eslint guard-for-in: 0*/
 
 var ExcludedClassProperties = ['__super__'];
-var ExcludedPrototypeProperties = ['constructor', 'extended'];
+var ExcludedPrototypeProperties = ['constructor'];
+
+function mix(from, to, exclude) {
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = Object.getOwnPropertyNames(from)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var name = _step.value;
+
+      if (exclude.indexOf(name) === -1) {
+        if (!to.hasOwnProperty(name)) {
+          var descriptor = Object.getOwnPropertyDescriptor(from, name);
+
+          Object.defineProperty(to, name, descriptor);
+        }
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+}
 
 var Mixin = (function () {
+  function Mixin() {
+    _classCallCheck(this, Mixin);
+  }
+
   _createClass(Mixin, null, [{
     key: 'includeInto',
     value: function includeInto(constructor) {
-      this.extend(constructor.prototype);
+      // include static properties
+      mix(this, constructor, ExcludedClassProperties);
 
-      for (var name in this) {
-        console.log('PROP', this.name);
-        if (ExcludedClassProperties.indexOf(name) === -1) {
-          if (!constructor.hasOwnProperty(name)) {
-            var descriptor = Object.getOwnPropertyDescriptor(constructor, name);
-
-            Object.defineProperty(constructor, name, descriptor);
-          }
-        }
-      }
+      // include instance properties
+      mix(this.prototype, constructor.prototype, ExcludedPrototypeProperties);
 
       if (this.included) {
         this.included(constructor);
       }
     }
-  }, {
-    key: 'extend',
-    value: function extend(object) {
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
-
-      try {
-        for (var _iterator = Object.getOwnPropertyNames(this.prototype)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var name = _step.value;
-
-          if (ExcludedPrototypeProperties.indexOf(name) === -1) {
-            if (!object.hasOwnProperty(name)) {
-              var descriptor = Object.getOwnPropertyDescriptor(this.prototype, name);
-
-              Object.defineProperty(object, name, descriptor);
-            }
-          }
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
-      }
-
-      if (this.prototype.extended) {
-        this.prototype.extended(object);
-      }
-    }
   }]);
-
-  function Mixin() {
-    _classCallCheck(this, Mixin);
-
-    if (this.extended) {
-      this.extended();
-    }
-  }
 
   return Mixin;
 })();
