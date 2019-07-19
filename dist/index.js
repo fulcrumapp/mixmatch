4be66f3781dff85fd92a46ff41a53f9cd26b2665
Zac McCormick <zac.mccormick@gmail.com>
2019-07-19 17:10:11 -0400

    Update babel deps and rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,75 +1,56 @@
-'use strict';
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+exports.__esModule = true;
+exports["default"] = void 0;
 
 /* eslint no-invalid-this: 0*/
-
 var ExcludedClassProperties = ['__super__'];
 var ExcludedPrototypeProperties = ['constructor'];
 
 function mix(from, to, exclude) {
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+  for (var _iterator = Object.getOwnPropertyNames(from), _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+    var _ref;
 
-  try {
-    for (var _iterator = Object.getOwnPropertyNames(from)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var name = _step.value;
-
-      if (exclude.indexOf(name) === -1) {
-        if (!to.hasOwnProperty(name)) {
-          var descriptor = Object.getOwnPropertyDescriptor(from, name);
-
-          Object.defineProperty(to, name, descriptor);
-        }
-      }
+    if (_isArray) {
+      if (_i >= _iterator.length) break;
+      _ref = _iterator[_i++];
+    } else {
+      _i = _iterator.next();
+      if (_i.done) break;
+      _ref = _i.value;
     }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
+
+    var name = _ref;
+
+    if (exclude.indexOf(name) === -1) {
+      if (!to.hasOwnProperty(name)) {
+        var descriptor = Object.getOwnPropertyDescriptor(from, name);
+        Object.defineProperty(to, name, descriptor);
       }
     }
   }
 }
 
-var Mixin = (function () {
-  function Mixin() {
-    _classCallCheck(this, Mixin);
-  }
+var Mixin =
+/*#__PURE__*/
+function () {
+  function Mixin() {}
 
-  _createClass(Mixin, null, [{
-    key: 'includeInto',
-    value: function includeInto(constructor) {
-      // include static properties
-      mix(this, constructor, ExcludedClassProperties);
+  Mixin.includeInto = function includeInto(constructor) {
+    // include static properties
+    mix(this, constructor, ExcludedClassProperties); // include instance properties
 
-      // include instance properties
-      mix(this.prototype, constructor.prototype, ExcludedPrototypeProperties);
+    mix(this.prototype, constructor.prototype, ExcludedPrototypeProperties);
 
-      if (this.included) {
-        this.included(constructor);
-      }
+    if (this.included) {
+      this.included(constructor);
     }
-  }]);
+  };
 
   return Mixin;
-})();
+}();
 
-exports.default = Mixin;
+exports["default"] = Mixin;
 
 for (var name in Mixin) {
   if (Mixin.hasOwnProperty(name)) {
